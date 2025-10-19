25fec6be5a67f01db7a3296566b1b305341852d7
Peshala Prabhapoorna <peshalaprabhapoorna@gmail.com>
2025-10-19 17:44:27 +0530

    style: apply Prettier formatting

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -1,6 +1,6 @@
-<footer class="footer sm:footer-horizontal footer-center text-base-content p-4">
-  <aside>
-    <p>Copyright © {new Date().getFullYear()} LavenderLeit</p>
-    <p class="text-xs">Powered by <a href="https://svelte.dev/">SvelteKit</a></p>
-  </aside>
+<footer class="footer-center footer px-4 py-8 text-base-content sm:footer-horizontal">
+	<aside>
+		<p>Copyright © {new Date().getFullYear()} LavenderLeit</p>
+		<p class="text-xs">Powered by <a href="https://svelte.dev/">SvelteKit</a></p>
+	</aside>
 </footer>
--- src/lib/components/navigation/Navbar.svelte
+++ src/lib/components/navigation/Navbar.svelte
@@ -1,11 +1,11 @@
-<div class="navbar shadow-sm py-6">
-  <div class="flex-1">
-    <a class="btn btn-ghost text-2xl" href="/">LavenderLeit</a>
-  </div>
-  <div class="flex-none">
-    <ul class="menu menu-horizontal px-1 text-base font-bold">
-      <li class="mx-2"><a href="/blog">Blog</a></li>
-      <li class="mx-2"><a href="/projects">Projects</a></li>
-    </ul>
-  </div>
+<div class="navbar py-6 shadow-sm">
+	<div class="flex-1">
+		<a class="btn text-2xl btn-ghost" href="/">LavenderLeit</a>
+	</div>
+	<div class="flex-none">
+		<ul class="menu menu-horizontal px-1 text-base font-bold">
+			<li class="mx-2"><a href="/blog">Blog</a></li>
+			<li class="mx-2"><a href="/projects">Projects</a></li>
+		</ul>
+	</div>
 </div>
